97a4d2583f73fbabe4977e51a0764162222c71c9
DmitryDemikhov <lp@lp>
2021-07-07 13:03:12 +0400

    Переустановка счётчика Яндекс Метрики

--- include/invis-counter.php
+++ include/invis-counter.php
@@ -57,16 +57,17 @@
             //------------------------------------//
 
             // <!-- Yandex.Metrika counter -->
+            console.log('ym');
             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?v=1", "ym");
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(22745254, "init", { 
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true,
-                    trackHash:true 
+            ym(22745254, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              ecommerce:"dataLayer"
             });
             // <!-- /Yandex.Metrika counter -->
 
