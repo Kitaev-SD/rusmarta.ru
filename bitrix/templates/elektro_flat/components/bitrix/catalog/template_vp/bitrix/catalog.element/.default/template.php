486e5cbc22965278b7af6f5abb60eb325800e784
DmitryDemikhov <lp@lp>
2021-11-08 17:06:00 +0400

    Редирект на каноникал

--- bitrix/templates/elektro_flat/components/bitrix/catalog/template_vp/bitrix/catalog.element/.default/template.php
+++ bitrix/templates/elektro_flat/components/bitrix/catalog/template_vp/bitrix/catalog.element/.default/template.php
@@ -42,6 +42,12 @@ $templateData = array(
     "JS_OBJ" => $strObName
 );
 
+// Редирект на canonical страницы
+$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if ($url != $arResult["CANONICAL_PAGE_URL"]) {
+    LocalRedirect($arResult["CANONICAL_PAGE_URL"]);
+}
+
 //JS//?>
 
 <?
