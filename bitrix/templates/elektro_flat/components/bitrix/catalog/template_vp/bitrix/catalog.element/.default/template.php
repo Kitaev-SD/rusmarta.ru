167430489ff182fed88d0c540531e398d7f85b15
DmitryDemikhov <lp@lp>
2021-11-10 17:16:52 +0400

    301 редирект на каноникал

--- bitrix/templates/elektro_flat/components/bitrix/catalog/template_vp/bitrix/catalog.element/.default/template.php
+++ bitrix/templates/elektro_flat/components/bitrix/catalog/template_vp/bitrix/catalog.element/.default/template.php
@@ -45,7 +45,7 @@ $templateData = array(
 // Редирект на canonical страницы
 $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 if ($url != $arResult["CANONICAL_PAGE_URL"]) {
-    LocalRedirect($arResult["CANONICAL_PAGE_URL"]);
+    LocalRedirect($arResult["CANONICAL_PAGE_URL"], false, '301 Moved permanently');
 }
 
 //JS//?>
